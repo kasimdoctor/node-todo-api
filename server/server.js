f9f9380583e26fe14f78aec58c8ee117d2031188
Kasim Doctor <kasim.doctor@appdirect.com>
2017-06-02 20:46:25 -0400

    Add POST api + refactor the code into modules

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const {mongoose} = require('./db/mongoose');
+const {Todo} = require('./models/todo.js');
+const {User} = require('./models/user.js');
+
+var app = express();
+
+// middleware for express to parse the request to JSON
+app.use(bodyParser.json());
+app.post('/todos', (request, response) => {
+    var todo = new Todo({
+        text: request.body.text,
+        completed: request.body.completed
+    });
+
+    todo.save().then((document) => {
+        response.send(document);
+    }, (error) => {
+        response.send(error);
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Started on port 3000');
+});
