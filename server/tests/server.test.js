68a66e03f737f9fabd322111fd16afe793ebfdbd
Kasim Doctor <kasim.doctor@appdirect.com>
2017-06-03 15:55:17 -0400

    Add tests for GET /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,12 +1,15 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
 const dummyTodos = [{
+    _id: new ObjectID(),
     text: 'First dummy todo'
 }, {
+    _id: new ObjectID(),
     text: 'Second dummy todo'
 }];
 
@@ -72,6 +75,33 @@ describe('GET /todos', () => {
             .expect(res => {
                 expect(res.body.todos.length).toBe(2);
             })
+            .end(done);
+    });
+});
+
+describe('GET /todos/:id', () => {
+    it('Should return TODO doc', (done) => {
+        request(app)
+            .get(`/todos/${dummyTodos[0]._id.toHexString()}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).toBe(dummyTodos[0].text);
+            })
+            .end(done);
+    });
+
+    it('Should return 404 if TODO not found', (done) => {
+        request(app)
+            .get(`/todos/${new ObjectID().toHexString()}`)
+            .expect(404)
+            .end(done);
+    });
+
+    it('Should return 404 for invalid ObjectIDs', (done) => {
+        request(app)
+            // passing an invalid id like 123
+            .get(`/todos/123`)
+            .expect(404)
             .end(done);
     });
 });
