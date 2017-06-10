b1bd96fb565c4fb0f3398c699a19b77b17f9a1f5
Kasim Doctor <kasim.doctor@appdirect.com>
2017-06-10 15:06:43 -0400

    Add DELETE /users/me/token route

--- server/server.js
+++ server/server.js
@@ -139,6 +139,14 @@ app.post('/users/login', (request, response) => {
     });
 });
 
+app.delete('/users/me/token', authenticate, (req, res) => {
+    req.user.removeToken(req.token).then(() => {
+        res.status(200).send();
+    }).catch(() => {
+        res.status(400).send();
+    });
+});
+
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
 });
